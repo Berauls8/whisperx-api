e838f88f860b0da8c15ac4af601abf3f8e40d829
Marcus Eibrink-Lunzenauer <lunzenauer@elan-ev.de>
2026-01-19 11:29:26 +0100

    Use `whisper` again in `transcription_utils.py`.

--- src/utils/transcription_utils.py
+++ src/utils/transcription_utils.py
@@ -9,13 +9,15 @@ def run_whisperx(temp_mp3_path, lang, model, min_speakers, max_speakers, prompt)
 
     # Start building the base command
     cmd = [
-        "whisperx",
+        #"whisperx",
+        "whisper",
         temp_mp3_path,
         "--model", model,
         "--language", lang,
         "--output_format", "all",
         "--output_dir", output_dir,
-        "--align_model", "WAV2VEC2_ASR_LARGE_LV60K_960H",
+        # whisperx option
+        #"--align_model", "WAV2VEC2_ASR_LARGE_LV60K_960H",
         "--verbose", "False"
     ]
 
